b400d6efb946e106c57c9d2830d35bba97f0c013
Matthias Munnich <munnich@env.ethz.ch>
2015-04-18 20:55:34 +0200

    Added WRITE_DEPTH and SLICE_AVG to averages.h.

--- src/averages.h
+++ src/averages.h
@@ -46,6 +46,80 @@ CSDISTRIBUTE_RESHAPE hbl_avg(BLOCK_PATTERN) BLOCK_CLAUSE
 CSDISTRIBUTE_RESHAPE hbbl_avg(BLOCK_PATTERN,*) BLOCK_CLAUSE
       common /avg_hbbl/hbbl_avg
 #  endif
-# endif
+!DL:
+#  ifdef WRITE_DEPTHS
+      real z_r_avg(GLOBAL_2D_ARRAY,N)
+      real z_w_avg(GLOBAL_2D_ARRAY,0:N), Hz_avg(GLOBAL_2D_ARRAY,N)
+      common /avg_depth/z_r_avg, z_w_avg, Hz_avg
+#  endif /* WRITE_DEPTHS */
+
+#  if defined BIOLOGY_NPZDOC && defined OXYGEN
+      real GasExcFlux_avg(GLOBAL_2D_ARRAY,NumGasExcTerms)
+      common /bgc_gasexcflux_avg/GasExcFlux_avg
+#  endif /* BIOLOGY_NPZDOC && OXYGEN */
+
+# endif /* SOLVE3D */
+!--> #endif /* AVERAGES */
+ 
+# ifdef SLICE_AVG
+      real zeta_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE zeta_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real ubar_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE  ubar_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real vbar_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE  vbar_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /slavg_zeta/zeta_slavg /slavg_ubar/ubar_slavg
+     &                          /slavg_vbar/vbar_slavg
+# ifdef SOLVE3D
+      real u_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE u_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real v_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE v_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real t_slavg(GLOBAL_2D_ARRAY,NT)
+CSDISTRIBUTE_RESHAPE t_slavg(BLOCK_PATTERN,*) BLOCK_CLAUSE
+      real rho_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE rho_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real w_slavg(GLOBAL_2D_ARRAY)
+      real wt_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE w_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /slavg_u/u_slavg /slavg_v/v_slavg /slavg_t/t_slavg
+     &                /slavg_rho/rho_slavg /slavg_w/w_slavg
+     &                /slavg_wt/wt_slavg
+      real akv_slavg(GLOBAL_2D_ARRAY)
+      real akt_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE akt_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /slavg_akv/akv_slavg /slavg_akt/akt_slavg
+#ifdef KPP_DIAGNOSE
+      real rich_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE rich_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real richN_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE richN_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      real swr_frac_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE swr_frac_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /rich_akt/rich_slavg, richN_slavg, swr_frac_slavg
+#endif
+#  ifdef SALINITY
+      real aks_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE aks_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /slavg_aks/aks_slavg
+#  endif
+#  ifdef LMD_KPP
+      real hbl_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE hbl_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /slavg_hbl/hbl_slavg
+#  endif
+#  ifdef LMD_BKPP
+      real hbbl_slavg(GLOBAL_2D_ARRAY)
+CSDISTRIBUTE_RESHAPE hbbl_slavg(BLOCK_PATTERN) BLOCK_CLAUSE
+      common /slavg_hbbl/hbbl_slavg
+#  endif
+
+#  if defined BIOLOGY_NPZDOC && defined OXYGEN
+      real GasExcFlux_slavg(GLOBAL_2D_ARRAY,NumGasExcTerms)
+      common /bgc_gasexcflux_avg/GasExcFlux_slavg
+#  endif /* BIOLOGY_NPZDOC && OXYGEN */
+
+# endif /* SOLVE3D */
+# endif /* SLICE_AVG */
 #endif /* AVERAGES */
  
