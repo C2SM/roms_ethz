e15c55d1b40da366d9cd005feb3b28bad8b98c0f
Domitille Louchard <dlouchar@citrus.ethz.ch>
2016-11-16 16:34:39 +0100

    adding ncjoin.F (from Damian) and adapting cppdefs.h

--- src/cppdefs_AMAZE_matt.h
+++ src/cppdefs_AMAZE_matt.h
@@ -7,45 +7,51 @@
 #include "cppdefs_UP.h"
 
      /* 1st  grid used  for amazon*/
-#define GRID_SIZE LLm=367, MMm=148, N=32
-#define  DOMAIN_TILING NP_XI=8, NP_ETA=4, NSUB_X=1, NSUB_E=1
+#define GRID_SIZE LLm=367, MMm=338, N=32
+#define  DOMAIN_TILING NP_XI=8, NP_ETA=8, NSUB_X=1, NSUB_E=1
 
      /* Forcing */
 
-!--#define SALINITY_MASK
+#define SALINITY_MASK
 !--SO # define SALINITY_MASKLATSTR -48.0
 !--SO # define SALINITY_MASKLATEND -53.01
 
-!-- #define TSOURCE
+#define TSOURCE
 !--SO #define ICEOBS
 
      /* Open Boundaries */
 !--SO #define EW_PERIODIC
 #define OBC_NORTH
-!--#define OBC_EAST
+#define OBC_EAST
 
      /* Open Boundary Conditions */
 #define OBC_M2FLATHER
-!--# define OBC_NORTH_M2SPECIFIED_TILESTR 1 /* OBC_M2SPECIFIED for a certain range of tiles */
-!--# define OBC_NORTH_M2SPECIFIED_TILEEND 367 /* OBC_M2SPECIFIED for a certain ran
+# define OBC_NORTH_M2SPECIFIED_TILESTR 1 /* OBC_M2SPECIFIED for a certain range of tiles */
+# define OBC_NORTH_M2SPECIFIED_TILEEND 367 /* OBC_M2SPECIFIED for a certain ran
 #define OBC_M3ORLANSKI /* Baroclin. BC: OBC_M3ORLANSKI, OBC_M3SPECIFIED */
 #define OBC_TORLANSKI /* Tracer BC: OBC_TORLANSKI, OBC_TSPECIFIED */
 
      /* Vertical Mixing */
-!-- #define LMD_LIMIT_STABLE / KPP fix for shallow mixing layer
+#define LMD_LIMIT_STABLE
 #define LMD_DDMIX
 #define LMD_BKPP
-  
-    /* Output */
+!--SO #define SO_AH16
+#ifdef SO_AH16
+# undef LMD_CONVEC
+# define LMD_MIN_KPP
+# define BRINE_PLUMES
+#endif
+
+     /* Output */
 #define AVERAGES
 #define SLICE_AVG
 !--SO #define WRITE_HEATFLX
-# define WRITE_SALT_REST
+!--SO #define WRITE_SALT_REST
 !--SO #define WRITE_TEMP_REST
 !--SO #define WRITE_CO2FLX
 
      /* Biology */
-!--#define BIOLOGY_BEC2
+#define BIOLOGY_BEC2
 #ifdef BIOLOGY_BEC2
 # define BEC_COCCO
 # define KILL_THE_WINNER  /* if defined, use Vallina 2014 parametrization for grazing */
@@ -64,4 +70,3 @@
 
 
 #include "set_global_definitions.h"
-
