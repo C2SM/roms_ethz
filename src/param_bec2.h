1280668ebc497c131dd73e84de052c0073750b4b
martinfr <martin.frischknecht@usys.ethz.ch>
2017-09-22 12:37:34 +0200

    Parameter adjustments for explicit sinking

--- src/param_bec2.h
+++ src/param_bec2.h
@@ -305,20 +305,24 @@
    !---------------------------------------------------------------------
        real wDustHard, wPOCHard, wPCaCO3Hard, wPSiO2Hard, wPIronHard
        real wDustSoft, wPOCSoft, wPCaCO3Soft, wPSiO2Soft, wPIronSoft
+       ! MF: First implementation
        !parameter( wDustHard=50.0,  wPOCHard=10., wPCaCO3Hard=50., wPSiO2Hard=50., wPIronHard=25. )
        !parameter( wDustSoft=50.0,  wPOCSoft=10., wPCaCO3Soft=50., wPSiO2Soft=50., wPIronSoft=25. )
+       !MF: Reduce sinking speeds
        parameter( wDustHard=25.0,  wPOCHard=25., wPCaCO3Hard=25., wPSiO2Hard=25., wPIronHard=25. )
        parameter( wDustSoft=25.0,  wPOCSoft=10., wPCaCO3Soft=25., wPSiO2Soft=25., wPIronSoft=25. )
        real param_dusthard_remin, param_pochard_remin, param_pcaco3hard_remin,
      &   param_psio2hard_remin, param_pironhard_remin
        real param_dustsoft_remin, param_pocsoft_remin, param_pcaco3soft_remin,
      &   param_psio2soft_remin, param_pironsoft_remin
+   ! MF: First implementation (match dissolution length scales of implicit sinking src code)
    !    parameter( param_dusthard_remin=8.0e-5, param_pochard_remin=0.00125, param_pcaco3hard_remin=0.00125,
    !  &   param_psio2hard_remin=0.00125, param_pironhard_remin=0.00125 )
    !    parameter( param_dustsoft_remin=8.0e-5, param_pocsoft_remin=0.125, param_pcaco3soft_remin=0.125,
    !  &   param_psio2soft_remin=0.125, param_pironsoft_remin=0.125 )
-       parameter( param_dusthard_remin=8.0e-5, param_pochard_remin=0.0025, param_pcaco3hard_remin=0.0025,
-     &   param_psio2hard_remin=0.0025, param_pironhard_remin=0.0025 )
-       parameter( param_dustsoft_remin=8.0e-5, param_pocsoft_remin=0.125, param_pcaco3soft_remin=0.25,
-     &   param_psio2soft_remin=0.25, param_pironsoft_remin=0.25 )
+   ! MF: Adjust to reduced sinking speeds
+       parameter( param_dusthard_remin=8.0e-5, param_pochard_remin=6.25e-4, param_pcaco3hard_remin=6.25e-4,
+     &   param_psio2hard_remin=6.25e-4, param_pironhard_remin=6.25e-4 )
+       parameter( param_dustsoft_remin=.125, param_pocsoft_remin=0.125, param_pcaco3soft_remin=6.25e-2,
+     &   param_psio2soft_remin=6.25e-2, param_pironsoft_remin=6.25e-2 )
 #endif
