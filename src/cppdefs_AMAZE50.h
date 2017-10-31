6779b5a7cc54c4556cc165e1b5fa564b22f19c7b
Domitille Louchard <domitille.louchard@usys.ethz.ch>
2017-10-31 16:35:44 +0100

    To make it compile with intel 2018

--- src/cppdefs_AMAZE50.h
+++ src/cppdefs_AMAZE50.h
@@ -3,65 +3,83 @@
   == === ====== ===== =====
   -- comments marked by SO are optins used in the Southern Ocean setup --
 */
-     /* Include standard CPP switches for UP ETH Zurich */
+
+#define BIOLOGY_BEC2
+
+/* Include standard CPP switches for UP ETH Zurich */
 #include "cppdefs_UP.h"
 
-     /* 1st  grid used  for amazon*/
-#define GRID_SIZE LLm=367, MMm=148, N=32
-#define  DOMAIN_TILING NP_XI=4, NP_ETA=8, NSUB_X=1, NSUB_E=1
 
-!--#define GRID_SIZE LLm=367, MMm=338, N=32
-!--#define  DOMAIN_TILING NP_XI=8, NP_ETA=8, NSUB_X=1, NSUB_E=1
+     /* Resolution*/
+
+#define FULLGRID /* CLIPPED140 CLIPPED210 */
+
+#ifdef CLIPPED140
+#define GRID_SIZE LLm=345, MMm=138, N=32
+#define  DOMAIN_TILING NP_XI=8, NP_ETA=8, NSUB_X=1, NSUB_E=1
+#endif
+#ifdef CLIPPED210
+#define GRID_SIZE LLm=345, MMm=208, N=32
+#define  DOMAIN_TILING NP_XI=8, NP_ETA=8, NSUB_X=1, NSUB_E=1
+#endif
+#ifdef FULLGRID
+#define GRID_SIZE LLm=345, MMm=331, N=32
+#define  DOMAIN_TILING NP_XI=8, NP_ETA=24, NSUB_X=1, NSUB_E=1
+#endif
 
      /* Forcing */
-
-!--#define SALINITY_MASK
+                     /*          - surface */
+#define SALINITY_MASK
 !--SO # define SALINITY_MASKLATSTR -48.0
 !--SO # define SALINITY_MASKLATEND -53.01
-
+#define VFLX_CORR
+!-- # define RIVER_LOAD_N
+!-- # define RIVER_LOAD_P
+!-- # define PCO2AIR_FORCING
+                     /*          - lateral */
 !--#define TSOURCE
-!--SO #define ICEOBS
+#define DEFAULT_BRY_VALUES
 
      /* Open Boundaries */
 !--SO #define EW_PERIODIC
 #define OBC_NORTH
-!--#define OBC_EAST
+#define OBC_EAST
+#define OBC_SOUTH
 
      /* Open Boundary Conditions */
-!--SO #define OBC_N_M2SPEC_STR 1 /* OBC_M2SPECIFIED for a certain range of tiles */
-!--SO #define OBC_N_M2SPEC_END 367 /* OBC_M2SPECIFIED for a certain ran*/
+#define OBC_S_M2SPEC_STR 1
+#define OBC_S_M2SPEC_END 12
+#define OBC_E_M2SPEC_STR 1
+#define OBC_E_M2SPEC_END 10
 
      /* Vertical Mixing */
-!-- #define LMD_LIMIT_STABLE / KPP fix for shallow mixing layer
+!--#define LMD_LIMIT_STABLE / KPP fix for shallow mixing layer
 #define LMD_DDMIX
 #define LMD_BKPP
-  
-    /* Output */
+
+     /* Biology */
+#ifdef BIOLOGY_BEC2
+# define BEC2_DIAG
+!--# define BEC_DDA
+!-- # define KILL_THE_WINNER  /* parametrization for grazing, not working for the moment */
+#endif
+
+     /* Other tracers */
+!-- #define PASSIVE_TRACER
+!-- #define AGE_DYE_TRACER
+
+     /* Output */
 #define AVERAGES
 !--#define SLICE_AVG
 !--SO #define WRITE_HEATFLX
-!-- #define WRITE_SALT_REST
-!#define WRITE_TEMP_REST
+#define WRITE_SALT_REST
+!--#define WRITE_TEMP_REST
 !--SO #define WRITE_CO2FLX
-!--#define FULL_PHYS_FLUX_ANALYSIS
 
-     /* Biology */
-!--#define BIOLOGY_BEC2
-#ifdef BIOLOGY_BEC2
-# undef BEC_COCCO
-# define KILL_THE_WINNER  /* if defined, use Vallina 2014 parametrization for grazing */
-# define BIOLOGY
-#endif
-
-!-- #define PASSIVE_TRACER
-!-- #define AGE_DYE_TRACER
-#define DEFAULT_BRY_VALUES
-
-!-- # define BEC2_DIAG
-!-- # define RIVER_LOAD_N
-!-- # define RIVER_LOAD_P
-!-- # define PCO2AIR_FORCING
-#define VFLX_CORR
+     /* Diagnostics */
+!--#define PHYS_FLUX_ANALYSIS
+!--# define KPP_DIAGNOSE
+!--SO #define COMPUTE_SPEED_DIAGNOSE
 
      /*Tides*/
 !--#define UV_TIDES
@@ -69,5 +87,9 @@
 
 !-- #define MMDEBUG
 
-#include "set_global_definitions.h"
 
+#include "set_global_definitions.h"
+!
+!  PARALLEL_FILES is set in set_global_definitions.h
+!  Switch it off, if not desired
+!-- #undef PARALLEL_FILES
