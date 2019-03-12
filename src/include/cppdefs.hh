4b6bb6200dff01c9cc5bf15427c5508cf1986001
Matthias Munnich <munnich@env.ethz.ch>
2019-03-12 19:00:27 +0100

    Sorted BGC models into different directories.

--- src/include/cppdefs.hh
+++ src/include/cppdefs.hh
@@ -1,9 +1,10 @@
-!--> #define CPPDEFS_FILE "cppdefs_AMAZON_TC.h"   /* Amazon River Mouth */
-!--> #define CPPDEFS_FILE "cppdefs_ATLTC.h"       /* Canary Telescopic */
-!--> #define CPPDEFS_FILE "cppdefs_MOOREA.h"      /* Moorea Island */
-!--> #define CPPDEFS_FILE "cppdefs_SO.h"          /* Southern Ocean */
-!--> #define CPPDEFS_FILE "cppdefs_HUMBPAC_TC.h"  /* Humboldt Current */
-!--> #define CPPDEFS_FILE "cppdefs_USWC.h"        /* US West Coast */
-#define CPPDEFS_FILE "cppdefs_PACTC.h"            /* US West Coast Telescopic */
+!--> #define CPPDEFS_FILE "cppdefs_AMACAN.h"   /* Amazon River Mouth (Domitille) */
+!--> #define CPPDEFS_FILE "cppdefs_ATLTC.h"    /* Canary Telescopic  (Elisa) */
+!--> #define CPPDEFS_FILE "cppdefs_MOONS.h"    /* Moorea NorthShore (Deniz) */
+!--> #define CPPDEFS_FILE "cppdefs_SO.h"       /* Southern Ocean (Cara, Alex) */
+!--> #define CPPDEFS_FILE "cppdefs_HUMBPAC.h"  /* Humboldt Current (Ana, Eike) */
+!--> #define CPPDEFS_FILE "cppdefs_USWC.h"     /* US West Coast (Zouhair) */
+!--> #define CPPDEFS_FILE "cppdefs_PACTC.h"    /* California Telescopic (Martin) */
+!--> #define CPPDEFS_FILE "cppdefs_CANAMA.h"   /* Canary-Amazonas (McKenzie) */
 
 #include CPPDEFS_FILE
