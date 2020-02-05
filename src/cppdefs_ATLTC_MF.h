4f375c49475da0941e47d100a88be8a9575a43f8
Flora Christine Julie Desmet <fdesmet@citrus.ethz.ch>
2020-02-05 16:58:45 +0100

    Copy newsrc_hc05 src code from MF tar folder

--- src/cppdefs_ATLTC_MF.h
+++ src/cppdefs_ATLTC_MF.h
@@ -6,24 +6,19 @@
      /* Include standard CPP switches for UP ETH Zurich */  
 #include "cppdefs_UP.h"
 
-#define GRID_SIZE LLm=602, MMm=516, N=64      ! pactcs30 4.1-65km telescopic up to Antarctica
-!-- #define DOMAIN_TILING NP_XI=8, NP_ETA=48, NSUB_X=1, NSUB_E=1 ! Euler
-#define DOMAIN_TILING NP_XI=4, NP_ETA=4, NSUB_X=1, NSUB_E=1 ! Euler
+#define GRID_SIZE LLm=811, MMm=395, N=64      ! atlel25 telescopic
+#define DOMAIN_TILING NP_XI=12, NP_ETA=32, NSUB_X=1, NSUB_E=1 ! Euler
+!#define DOMAIN_TILING NP_XI=4, NP_ETA=8, NSUB_X=1, NSUB_E=1 ! Euler
 
      /* Open Boundaries */
+#define OBC_WEST
 #define OBC_SOUTH
 
      /* Open Boundary Conditions */
 !-- Try to treat inflow/outflow of ACC differently! (not yet tested, so far I used M2FLATHER)
 !-- new only indic/pacific, see below #define OBC_M2SPECIFIED /* special for SO */
-#define OBC_S_M2SPEC_STR 000 /* OBC_M2SPECIFIED for a certain range of tiles */
-#define OBC_S_M2SPEC_END 004 /* OBC_M2SPECIFIED for a certain range of tiles */
 #define OBC_M3ORLANSKI /* Baroclin. BC: OBC_M3ORLANSKI, OBC_M3SPECIFIED */
 #define OBC_TORLANSKI /* Tracer BC: OBC_TORLANSKI, OBC_TSPECIFIED */
-
-! MF 11/11/2016: Run without sponge as I have before...
-#undef SPONGE
-#undef SPONGE_WIDTH 
 
 
      /* Open Boundary Conditions */
@@ -32,24 +27,27 @@
 
     /*  Use CLM Files */
 !-- #ifdef CLMFORCING   
-!-- # define TCLIMATOLOGY
+!# define TCLIMATOLOGY
 /* Partial restoring of TS in user-defined region via 2D field nudg_weights in clm file (MF)*/
-!-- # define TNUDGE_WEIGHTS /* TCLIMATOLOGY must be defined */
-!-- # define UCLIMATOLOGY
-!-- # define TNUDGING
-!-- # define M3NUDGING
-!-- # define M2NUDGING
+!# define TNUDGE_WEIGHTS /* TCLIMATOLOGY must be defined */
+!--> # define UCLIMATOLOGY
+!--> # define TNUDGING
+!--> # define M3NUDGING
+!--> # define M2NUDGING
 # ifdef TCLIMATOLOGY
 #      define CLIMAT_TS_MIXH
 # endif
 !--   /* undefine default BRY settings */
-!-- # undef T_FRC_BRY
-!-- # undef Z_FRC_BRY
-!-- # undef M3_FRC_BRY
-!-- # undef M2_FRC_BRY
+!# undef T_FRC_BRY
+!# undef Z_FRC_BRY
+!# undef M3_FRC_BRY
+!# undef M2_FRC_BRY
 !--> #else                /* Default: Use BRY Files*/
 !--> # undef SPONGE /* note: UP ETH before 8/2015 did not use SPONGE with BRY */
 !-- #endif
+
+    /* Bottom KPP */
+#define LMD_BKPP
 
 
 !--> #undef OBC_M2FLATHER  /* undefine default in cppdefs_UP.h */
@@ -62,7 +60,7 @@
      /* Output */
 #define AVERAGES
 !#define SLICE_AVG
-#define CALENDAR '365_day'     /* netCDF CF-convention CALENDAR attribute default: '360_day' */
+#define CALENDAR '360_day'     /* netCDF CF-convention CALENDAR attribute default: '360_day' */
 !--> #define STARTDATE '0001-01-01' /* part of netCDF CF-convention time units attribute default: '0001-01-01'*/
 
 !--> #define ADV_ISONEUTRAL
@@ -72,16 +70,15 @@
 # define BIOLOGY_BEC2
 
 #ifdef BIOLOGY_BEC2
-# define BIOLOGY
-# define DEFAULT_BRY_VALUES
+#  define BIOLOGY
+!#  define USE_EXPLICIT_VSINK
+#  define DEFAULT_BRY_VALUES
 !--> # define MULT_CLIM_FILES
-# define VFLX_CORR
-!--> # define BEC2_DIAG
-!-- # define RIVER_LOAD_N
-!-- # define RIVER_LOAD_P
-!-- # define NHY_FORCING
-!-- # define NOX_FORCING
-!-- # define PCO2AIR_FORCING
+!--> # define VFLX_CORR
+# define BEC2_DIAG
+!# define RIVER_LOAD_N
+!# define RIVER_LOAD_P
+!--> # define PCO2AIR_FORCING
 #endif /* BIOLOGY_BEC2 */
 
 
