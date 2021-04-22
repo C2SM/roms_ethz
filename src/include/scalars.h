4a3d179ee08d22f027dcf3fd6e10300edecd1c6f
Matthieu <matthieu.leclair@env.ethz.ch>
2021-04-22 15:42:19 +0200

    FIX: temporary fix for the coupled model to compile
    It probably needs a cleanner fix

--- src/include/scalars.h
+++ src/include/scalars.h
@@ -109,11 +109,11 @@ C$OMP THREADPRIVATE(/priv_scalars/)
       common /scalars_main/ n_sponge, v_sponge
 #endif
 
-# if defined OBC_M2ORLANSKI && ( defined M2_FRC_BRY \
+!! # if defined OBC_M2ORLANSKI && ( defined M2_FRC_BRY \
                                || defined M2NUDGING )
       real attnM2
       common /scalars_main/ attnM2
-# endif
+!! # endif
 
 
 
